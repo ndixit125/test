cd7ddf2ffa0fe98f94f2dc027c3edec89db1c633
ndixit125 <149649101+ndixit125@users.noreply.github.com>
2024-04-09 19:58:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from emailData import EmailData
+from transformers import pipeline
+import google.generativeai as genai
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+tempData = []
+
+@app.get("/getemaildetails")
+async def get_data(request: Request):
+    data = await request.json()
+    return {"Data": data}
+
+#nlp = pipeline(task='sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
+
+text = 'Hi Nikunj, Following up on this issue. Please let us know of the status. Sincerely, Nikunj'
+#print(f'{nlp(text)}')
+
+genai.configure(api_key="AIzaSyBwKOUT0DaLHUaqdtWyCNRqU8zgeBwGiT4")
+
+defaults = {
+ 'model': 'models/text-bison-001',
+ 'temperature': 0.5,
+ 'candidate_count': 1,
+ 'top_k': 40,
+ 'top_p': 0.95,
+ 'max_output_tokens': 1024,
+}
+prompt =f"""What is the sentiment of the following sentence, which is delimited with triple backticks? Just give the sentiment in one word if it is positive, very positive, negative, very negative or neutral
+Review text: '''{text}'''"""
+response = genai.generate_text(**defaults, prompt=prompt)
+tempData.append(response.result)
+output = tempData[0]
+print(output)
+
+@app.get("/SentimentAnalysis")
+async def get_analysis():
+    return {"Sentiment": output}
